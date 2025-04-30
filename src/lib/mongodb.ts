fb5a70d0df0660fc5943805e7d0a67c48e34c0ba
Perfecto II Cayabyab <perfectocayabyab@gmail.com>
2025-05-01 02:57:35 +0800

    Fix mongodb eslint v3

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,23 +6,24 @@ const options = {};
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-// Extend NodeJS.Global to include our custom property
-declare global {
-  // eslint-disable-next-line no-var
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+// Custom global type (Next.js doesn't allow implicit `any` here)
+interface GlobalWithMongoClientPromise {
+  _mongoClientPromise?: Promise<MongoClient>;
 }
+
+// Extend the globalThis object
+const globalWithMongo = globalThis as typeof globalThis & GlobalWithMongoClientPromise;
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your MongoDB URI to .env.local');
 }
 
 if (process.env.NODE_ENV === 'development') {
-  // Use global variable to preserve value across HMR in dev
-  if (!global._mongoClientPromise) {
+  if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    globalWithMongo._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
